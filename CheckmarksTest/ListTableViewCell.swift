3c2e85ab6bb6068ff9bc408e73c15f6973dfe9e9
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-06-05 14:07:43 +0900

    Create checkBoxToggle function

--- CheckmarksTest/ListTableViewCell.swift
+++ CheckmarksTest/ListTableViewCell.swift
@@ -7,9 +7,19 @@
 
 import UIKit
 
+
+protocol ListTableViewCellDelegate: class {
+    func checkBoxToggle(sender: ListTableViewCell)
+}
+
 class ListTableViewCell: UITableViewCell {
 
+    weak var delegate: ListTableViewCellDelegate?
+    
     @IBOutlet weak var checkBoxButton: UIButton!
     
-
+    @IBAction func checkToggled(_ sender: UIButton) {
+        delegate?.checkBoxToggle(sender: self)
+    }
+    
 }
--- CheckmarksTest/ViewController.swift
+++ CheckmarksTest/ViewController.swift
@@ -13,9 +13,14 @@ class ViewController: UIViewController {
     
     var toDoItems : [ToDoItem] = []
     
+    var toDoItem: ToDoItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if toDoItem == nil {
+            toDoItem = ToDoItem(completed: false)
+        }
         
         testTableView.delegate = self
         testTableView.dataSource = self
@@ -26,13 +31,28 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension ViewController: UITableViewDelegate, UITableViewDataSource, ListTableViewCellDelegate {
+    
+    func checkBoxToggle(sender: ListTableViewCell) {
+        if let selectedIndexPath = testTableView.indexPath(for: sender){
+            
+            print("push\(selectedIndexPath.row)")
+            
+//            toDoItems[selectedIndexPath.row].completed = !toDoItems[selectedIndexPath.row].completed
+           testTableView.reloadRows(at: [selectedIndexPath], with: .automatic)
+
+        }
+        
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = testTableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! ListTableViewCell
+        
+        cell.delegate = self
         
         cell.checkBoxButton.isSelected = false
         
